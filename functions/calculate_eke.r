9f3950c8e438eb07beca29bbfd55d7dba0fb8248
nfarchadi <nfarchadi@sdsu.edu>
2022-12-29 23:48:18 -0800

    remove folder

--- functions/calculate_eke.r
+++
@@ -1,5 +0,0 @@
-# function to calculate eddy kinetic energy (eke)
-
-calculate_eke <- function(x) {
-  (x[[1]] ^ 2 + x[[2]] ^ 2) / 2
-}
--- functions/calculate_ild.r
+++
@@ -1,8 +0,0 @@
-#function to calcualte isothermal layer depth (ILD)
-fun <- function(x) {
-  if (all(is.na(x))){
-    NA
-  } else{
-    dep[which.min(abs(x[1] - x[2:40] - 0.5))]
-  }
-} 
